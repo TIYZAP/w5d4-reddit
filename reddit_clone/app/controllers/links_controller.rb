e36c47b2232550d020366f5605fc7469fe132b83
TIYZAP <ironyardzap@gmail.com>
2016-10-27 18:04:47 -0400

    Easy Mode

---
+++ reddit_clone/app/controllers/links_controller.rb
@@ -0,0 +1,40 @@
+class LinksController < ApplicationController
+
+
+  def index
+    @links = Link.all.order(votes: :desc)
+  end
+
+
+  def new
+    @links = Link.new
+  end
+
+  def create
+    @links = Link.new(links_params)
+    if @links.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def upvote
+    @links = Link.find(params[:id])
+    @links.votes +=1
+    @links.save
+    redirect_to root_path
+  end
+
+  def downvote
+    @links = Link.find(params[:id])
+    @links.votes -=1
+    @links.save
+    redirect_to root_path
+  end
+
+  def links_params
+    params.require(:link).permit(:title, :url, :summary)
+  end
+
+end
