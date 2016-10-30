31bc6524da31f3d55792999edda6a7cb75c87b4b
TIYZAP <ironyardzap@gmail.com>
2016-10-30 17:44:21 -0400

    Exp Adv Conplete

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-
-
+  
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new (user_params)
     if @user.save
       redirect_to root_path
     else
